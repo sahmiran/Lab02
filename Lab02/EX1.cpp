981357a1328b5cf2a6e400a47d539b1004090dbe
Hasan Can <Hasan Can@DESKTOP-G2UL1BP>
2022-03-15 20:28:35 +0300

    Add project files.

---
+++ Lab02/EX1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+using namespace std;
+
+void main() {
+
+	int A[] = {1,2,3,4,5,6,7,8,9}; //onceden tanimli dizi
+	int N; //ne kadar kaydirilacagini tutan degisken
+	char direction; //hangi yone kayma yapilacagini belirleyen degisken
+
+	cout << "Diziyi kac birim kaydirmak istiyorsunu" << endl;
+	cin >> N;  //N degeri kullanicidan istendi
+
+	cout << "Hangi yone kaydirma islemi yapilsin     Sag = r  ----- Sol = l" << endl;
+	cin >> direction;     // direction bilgisi kullanicidan istendi
+
+
+}
